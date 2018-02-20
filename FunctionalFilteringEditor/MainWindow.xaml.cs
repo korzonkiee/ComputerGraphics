730a9dfe685b442de2c9c2263b7bcab8f39dec13
Korzonkie <rootie0011@gmail.com>
2018-02-20 20:40:36 +0100

    Draw axis

--- FunctionalFilteringEditor/MainWindow.xaml.cs
+++ FunctionalFilteringEditor/MainWindow.xaml.cs
@@ -20,9 +20,52 @@ namespace FunctionalFilteringEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Line xAxis;
+        private Line yAxis;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            drawAxis();
+        }
+
+        private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var menuItem = new MenuItem();
+            menuItem.Header = "MenuItem";
+            var contextMenu = new ContextMenu();
+            contextMenu.Items.Add(menuItem);
+            canvas.ContextMenu = contextMenu;
+        }
+
+        private void addTextBoxMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void drawAxis()
+        {
+            xAxis = new Line();
+            yAxis = new Line();
+
+            xAxis.Stroke = Brushes.Black;
+            yAxis.Stroke = Brushes.Black;
+
+            xAxis.X1 = 0;
+            xAxis.X2 = 255;
+            xAxis.Y1 = 0;
+            xAxis.Y2 = 0;
+
+            yAxis.X1 = 0;
+            yAxis.X2 = 0;
+            yAxis.Y1 = 0;
+            yAxis.Y2 = 255;
+
+            xAxis.StrokeThickness = 3;
+            yAxis.StrokeThickness = 3;
+
+            canvas.Children.Add(xAxis);
+            canvas.Children.Add(yAxis);
         }
     }
 }
